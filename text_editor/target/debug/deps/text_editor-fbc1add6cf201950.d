c1868ef3698298c953f7057d1cca48e9d5fd775b
Typhoon1551 <141539087+Typhoon1551@users.noreply.github.com>
2023-11-21 16:21:31 +0000

    added cargo project

---
+++ text_editor/target/debug/deps/text_editor-fbc1add6cf201950.d
@@ -0,0 +1,5 @@
+/workspaces/text-editor/text_editor/target/debug/deps/libtext_editor-fbc1add6cf201950.rmeta: src/main.rs
+
+/workspaces/text-editor/text_editor/target/debug/deps/text_editor-fbc1add6cf201950.d: src/main.rs
+
+src/main.rs:
