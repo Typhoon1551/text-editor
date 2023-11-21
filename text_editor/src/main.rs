9598d5eaf62ecebef813e6319774232fe73510c1
Typhoon1551 <141539087+Typhoon1551@users.noreply.github.com>
2023-11-21 16:40:04 +0000

    added screen print

--- text_editor/src/main.rs
+++ text_editor/src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
+
+const X_SIZE:usize = 100;
+const Y_SIZE:usize = 10;
+const FILL_CHAR:char = '_';
+
+struct Position (u32,u32);
+
 fn main() {
-    println!("Hello, world!");
+    let mut cursor = Position(32,5);
+    let mut screen = [[FILL_CHAR;X_SIZE];Y_SIZE];
+
+    screen[cursor.1 as usize][cursor.0 as usize] = '%';
+
+    print_screen(screen);
+}
+
+fn print_screen(screen:[[char;X_SIZE];Y_SIZE]) {
+    for row in screen {
+        for col in row {
+            print!("{}", col);
+        }
+        println!();
+    }
 }
