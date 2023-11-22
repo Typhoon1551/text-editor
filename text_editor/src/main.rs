f791661d8c8b3386081bd562775c3c31c564d46d
Typhoon1551 <tylervarnum9@gmail.com>
2023-11-22 00:59:04 -0500

    updated drawing teqnique

--- text_editor/src/main.rs
+++ text_editor/src/main.rs
@@ -2,39 +2,50 @@
 use std::io;
 
 //constants
-const X_SIZE:usize = 100;
-const Y_SIZE:usize = 10;
-const FILL_CHAR:char = ' ';
+const SIZE:usize = 10;
+const CURSOR_CHAR:char = '%';
 
 //structs
 
 fn main() {
-    let mut cursor: u32 = 3;
-    let mut screen = [[FILL_CHAR;X_SIZE];Y_SIZE];
+    let mut cursor: u8 = 0;
+    let mut screen = vec![String::new();256];
 
-    //loop {
+    loop {
         print_screen(&mut screen,&cursor);
 
         let mut input: String = String::new();
         io::stdin().read_line(&mut input).expect("Failed to read line");
 
         let input = input.trim();
-    //}
+
+        if input.chars().nth(0).unwrap() == '\\' {
+            if input.chars().nth(1).unwrap() == '\\'{
+                match input.trim() {
+                    "\\\\quit" => break,
+                    &_ => println!("Unknown command"),
+                }
+            } else {
+                cursor = (input as &str)[1..].parse::<u8>().expect("invalid line number");
+            }
+        } else {
+            screen[cursor as usize] = input.to_string();
+            cursor += 1;
+        }
+    }
 }
 
-fn print_screen(screen:&mut [[char;X_SIZE];Y_SIZE], cursor:&u32) {
-    println!("{}{}{}","+---+","-".repeat(X_SIZE),'+');
+fn print_screen(screen:&mut Vec<String>, cursor:&u8) {
+    println!("{}{}{}","+---+","-".repeat(100),'+');
 
-    let mut line:u32 = 0;
+    let mut number:u8 = 0;
+    let screen_top = (cursor - (SIZE as u8/2)).clamp(0, 255) as usize;
 
-    for row in screen {
-        print!("|{: >3}|",if line == cursor.to_owned() {"%%%".to_string()} else {line.to_string()});
-        for col in row {
-            print!("{}", col);
-        }
-        println!("|");
-        line += 1;
+    for line in &screen[screen_top..screen_top+SIZE] {
+        println!("|{: >3}|{: <100}|",if number == cursor.to_owned() {CURSOR_CHAR.to_string().repeat(3)} else {number.to_string()}, line);
+        number += 1;
     }
 
-    println!("{}{}{}","+---+","-".repeat(X_SIZE),'+');
+    println!("{}{}{}","+---+","-".repeat(100),'+');
+    print!("{}:",number)
 }
