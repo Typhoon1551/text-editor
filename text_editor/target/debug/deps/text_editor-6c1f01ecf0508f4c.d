4979f36b65186d4dac43e709e5e5aa48bd1d9f9b
Tyler Varnum <tylervarnum9@gmail.com>
2025-11-10 03:17:36 -0500

    fixed cooked-ass file structure

--- text_editor/target/debug/deps/text_editor-6c1f01ecf0508f4c.d
+++
@@ -1,5 +0,0 @@
-/workspaces/text-editor/text_editor/target/debug/deps/libtext_editor-6c1f01ecf0508f4c.rmeta: src/main.rs
-
-/workspaces/text-editor/text_editor/target/debug/deps/text_editor-6c1f01ecf0508f4c.d: src/main.rs
-
-src/main.rs:
--- text_editor/target/debug/deps/text_editor-fbc1add6cf201950.d
+++
@@ -1,5 +0,0 @@
-/workspaces/text-editor/text_editor/target/debug/deps/libtext_editor-fbc1add6cf201950.rmeta: src/main.rs
-
-/workspaces/text-editor/text_editor/target/debug/deps/text_editor-fbc1add6cf201950.d: src/main.rs
-
-src/main.rs:
